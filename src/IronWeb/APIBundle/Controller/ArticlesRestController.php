a6cc785b9b5a4ce9c9da0ce2276700df7d63467c
MONDOT Quentin <quent.mondot@hotmail.fr>
2016-02-16 09:37:30 +0000

    Function rate an article

--- src/IronWeb/APIBundle/Controller/ArticlesRestController.php
+++ src/IronWeb/APIBundle/Controller/ArticlesRestController.php
@@ -3,14 +3,15 @@
 namespace IronWeb\APIBundle\Controller;
 
 use IronWeb\APIBundle\Entity\Article;
+use IronWeb\APIBundle\Entity\ArticleRate;
 use IronWeb\APIBundle\Form\ArticleType;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 
 class ArticlesRestController extends FOSRestController
@@ -54,6 +55,41 @@ class ArticlesRestController extends FOSRestController
         // created => 200
         return new Response("Congratulations!");
         }
+
+    }
+
+  public function putArticleRateAction($id,$rate)
+    {
+
+      $em = $this->getDoctrine()->getManager();
+
+      $article = $em
+        ->getRepository('IronWebAPIBundle:Article')
+        ->find($id)
+      ;
+
+      if (null == $article) {
+        throw new NotFoundHttpException("The article with id ".$id." does not exist.");
+      }
+
+      $articleRate = new ArticleRate();
+      $articleRate->setArticle($article);
+      $articleRate->setRate($rate);
+
+      // $validator = $this->get('validator');
+      // $listErrors = $validator->Validate($articleRate);
+
+      // if ($listErrors==0) {
+      // 	$manager = $this->getDoctrine()->getManager();
+      // 	$manager->persist($articleRate);
+      // 	$manager->flush();
+      // }
+
+      // return new Response("Congratulations! the number of errors is $listErrors");
+
+      $manager = $this->getDoctrine()->getManager();
+      $manager->persist($articleRate);
+      $manager->flush();
 
     }
 }
